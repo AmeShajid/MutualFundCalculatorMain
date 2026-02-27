ba6b24e259703693b8ee15d7701c73045add10fb
Ame Shajid <ameshajid@gmail.com>
2026-02-27 01:39:14 -0500

    Initial Spring Boot backend setup

---
+++ src/main/java/com/mutualfundcalculator/MutualFundCalculatorMainApplication.java
@@ -0,0 +1,13 @@
+package com.mutualfundcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualFundCalculatorMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutualFundCalculatorMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mutualfundcalculator/MutualFundCalculatorMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutualfundcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutualFundCalculatorMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
